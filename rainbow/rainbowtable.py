1518a612110c474147bbcaa2508f15fa82ab6c9e
Seungjin Lee <ronaldojin14@gmail.com>
2021-09-15 17:40:46 +0900

    second commit

--- rainbow/rainbowtable.py
+++ rainbow/rainbowtable.py
@@ -2,11 +2,9 @@ from hashlib import sha1
 from multiprocessing import Process
 
 START, END = 10000000, 100000000
-PARTS = 10 #프로세스 개수
+PARTS = 10
 SIZE = (END-START)//PARTS
 
-#hash_i.csv 파일에 (원본 값), (해시 값)의 형태로 저장
-#hash_0.csv ~ hash_(PARTS-1).csv 파일에 나누어 저장
 def rainbow(i):
 	start = START + SIZE * i
 	end = start + SIZE
